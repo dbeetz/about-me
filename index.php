555b221128620b978653621eedb04cf707855587
Devon Beets <dbeetzz@gmail.com>
2016-07-12 16:39:46 -0600

    Added first paragraph to main

--- index.php
+++ index.php
@@ -9,4 +9,9 @@
 	<body>
 		<h1>Web Developer in Training</h1>
 	</body>
+
+	<!-- Start Main Content -->
+	<main>
+		<p id="first paragraph">My name is Devon Beets, and I am a web developer in training at the Deep Dive Coding Bootcamp! I am fabricating this webpage for practice purposes and to get exposure to coding in HTML, CSS, and JavaScript! :) I am looking forward to learning as much as I can about these languages and others in the weeks to come!</p>
+	</main>
 </html>
