1c9d871fca497301105ba0db9ed68c037304faf6
Devon Beets <dbeetzz@gmail.com>
2016-07-12 16:51:12 -0600

    Added js and css and linked to index

--- index.php
+++ index.php
@@ -3,15 +3,22 @@
 	<head>
 		<meta charset="utf-8" />
 
+		<!-- Adding Custom CSS -->
+		<link href="css/style.css" rel="stylesheet" type="text/css" />
+
+		<!-- Adding Custom JavaScript -->
+		<script src="js/custom.js" type="text/javascript"></script>
+
 		<!-- Page Title -->
 		<title>About Me, Devon Beets!</title>
 	</head>
 	<body>
-		<h1>Web Developer in Training</h1>
-	</body>
+		<h1>Devon Beets: Web Developer in Training</h1>
+
 
 	<!-- Start Main Content -->
 	<main>
 		<p id="first paragraph">My name is Devon Beets, and I am a web developer in training at the Deep Dive Coding Bootcamp! I am fabricating this webpage for practice purposes and to get exposure to coding in HTML, CSS, and JavaScript! :) I am looking forward to learning as much as I can about these languages and others in the weeks to come!</p>
 	</main>
+	</body>
 </html>
