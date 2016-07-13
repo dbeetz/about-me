c43860d0a43eb5febdb81f06aa14bea9bd455049
Devon Beets <dbeetzz@gmail.com>
2016-07-12 19:38:22 -0600

    Added comments for picture and video

--- index.php
+++ index.php
@@ -21,9 +21,11 @@
 
 	<!-- Start Main Content -->
 	<main>
+		<!-- Insert Picture of Self -->
 		<img src="images/DevonBeetsSelfieHalfSize.png" alt="Devon Beets" />
 		<p id="first paragraph">My name is Devon Beets, and I am a web developer in training at the Deep Dive Coding Bootcamp! I am fabricating this webpage for practice purposes and to get exposure to coding in HTML, CSS, and JavaScript! :) I am looking forward to learning as much as I can about these languages and others in the weeks to come!</p>
 		<p id="second paragraph">To expand upon myself, I love my family, friends, and several hobbies including playing guitar, soccer, and snowboarding! Here next is a short clip of my friend and I snowboarding in Angel Fire, New Mexico:</p>
+		<!-- Insert Video -->
 		<iframe width="560" height="315"
 				  src="https://www.youtube.com/embed/kS4vE217gI4" frameborder="0" allowfullscreen>
 		</iframe>
