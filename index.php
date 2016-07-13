2c93f7406c4cdb44b1c4ca1e2bc54c0ddcefbd8f
Devon Beets <dbeetzz@gmail.com>
2016-07-13 09:01:14 -0600

    added divs and customized them with css

--- index.php
+++ index.php
@@ -15,23 +15,32 @@
 	<body>
 
 		<!-- Begin Header -->
+		<div class="name">
 		<header>
 		<h1>Devon Beets</h1>
 		</header>
+		</div>
 
 	<!-- Start Main Content -->
 	<main>
+
+		<h2>About Me</h2>
+		<!-- Organize content with Div -->
+		<div class="photo-and-text">
 
 		<!-- Insert Picture of Self -->
 		<img src="images/DevonBeetsSelfieHalfSize.png" alt="Devon Beets" />
 
 		<!-- Begin Text -->
-		<p id="first paragraph">Hello there! <br/> My name is Devon Beets, and I am a web developer in training at the Deep Dive Coding Bootcamp at CNM! I am fabricating this webpage for practice purposes and to get exposure to coding in HTML, CSS, and JavaScript! :) I am looking forward to learning as much as I can about these languages and others in the weeks to come!</p>
-		<p id="second paragraph">To expand upon myself, I love my family, friends, and several hobbies including playing guitar, soccer, and snowboarding! Here next is a short clip of my friend and I snowboarding in Angel Fire, New Mexico:</p>
+		<p id="first paragraph">Hello there! <br/> My name is Devon Beets, and I am a web developer in training at the Deep Dive Coding Bootcamp at CNM! I am fabricating this webpage for practice purposes and to get exposure to coding in HTML, CSS, and JavaScript! :) I am looking forward to learning as much as I can about these languages and others in the weeks to come! To expand upon myself, I love my family, friends, and several hobbies including playing guitar, soccer, and snowboarding! Here next is a video of my friend and I snowboarding in Angel Fire, New Mexico:</p>
+		</div>
+
 		<!-- Insert Video -->
+		<div class="video">
 		<iframe width="560" height="315"
 				  src="https://www.youtube.com/embed/kS4vE217gI4" frameborder="0" allowfullscreen>
 		</iframe>
+		</div>
 	</main>
 
 		<!-- Begin Footer-->
@@ -44,7 +53,7 @@
 				<li>dbeets@cnm.edu</li>
 				<li>(505) 280-5614</li>
 			</ul>
-			&copy; 2016
+			&copy; 2016 dbeets design
 		</footer>
 	</body>
 </html>
