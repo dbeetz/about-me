c02db681e3a9b90f3b6bbb2ce4069e39a7b9f679
Devon Beets <dbeetzz@gmail.com>
2016-07-12 21:05:11 -0600

    Removed head css and added background gradient to body

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 		<img src="images/DevonBeetsSelfieHalfSize.png" alt="Devon Beets" />
 
 		<!-- Begin Text -->
-		<p id="first paragraph">Hello there! <br/> My name is Devon Beets, and I am a web developer in training at the Deep Dive Coding Bootcamp! I am fabricating this webpage for practice purposes and to get exposure to coding in HTML, CSS, and JavaScript! :) I am looking forward to learning as much as I can about these languages and others in the weeks to come!</p>
+		<p id="first paragraph">Hello there! <br/> My name is Devon Beets, and I am a web developer in training at the Deep Dive Coding Bootcamp at CNM! I am fabricating this webpage for practice purposes and to get exposure to coding in HTML, CSS, and JavaScript! :) I am looking forward to learning as much as I can about these languages and others in the weeks to come!</p>
 		<p id="second paragraph">To expand upon myself, I love my family, friends, and several hobbies including playing guitar, soccer, and snowboarding! Here next is a short clip of my friend and I snowboarding in Angel Fire, New Mexico:</p>
 		<!-- Insert Video -->
 		<iframe width="560" height="315"
