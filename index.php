8146bad2fc82fd50d78ecf930258f87abdf3ae0f
Devon Beets <dbeetzz@gmail.com>
2016-07-15 08:48:07 -0600

    Added db favicon

--- index.php
+++ index.php
@@ -9,13 +9,16 @@
 		<!-- Adding Custom JavaScript -->
 		<script src="js/custom.js" type="text/javascript"></script>
 
+		<!-- Adding a Favicon -->
+		<link type="image/ico" href="images/dbfavicon.ico" rel="shortcut icon" />
+
 		<!-- Page Title -->
 		<title>About Me, Devon Beets!</title>
 	</head>
 	<body>
 
 		<!-- Begin Header -->
-		<div class="name">
+		<div class="container">
 		<header>
 		<h1>Devon Beets</h1>
 		</header>
@@ -34,9 +37,7 @@
 		<!-- Begin Text -->
 			<p><strong>Hello there! </strong><br/> My name is Devon Beets, and I am a web developer in training at the Deep Dive Coding Bootcamp at CNM! I am fabricating this webpage for practice purposes and to get exposure to coding in HTML, CSS, and JavaScript! :) I am looking forward to learning as much as I can about these languages and others in the weeks to come! To expand upon myself, I love my family, friends, and several hobbies including playing guitar, soccer, and snowboarding! Here next is a video of my friend and I snowboarding in Angel Fire, New Mexico:</p>
 		</div>
-		<div class ="second-paragraph">
-			<p> </p>
-		</div>
+		
 		<!-- Insert Video -->
 		<div class="video">
 		<iframe width="560" height="315"
